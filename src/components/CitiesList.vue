ac68f38c33d7fea881e0006b05ae86e455f24f26
Martin BRANDEL <martin.brandel@Digital-Grenoble.local>
2022-04-26 11:53:15 +0200

    ajout exercices 1 à 4

---
+++ src/components/CitiesList.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="hello">
+    <CityComponent :name="cities[0].name">
+      
+    </CityComponent>
+  </div>
+</template>
+
+<script>
+
+import CityComponent from './City.vue'
+import { format } from '/node_modules/timeago.js';
+
+export default {
+  components: { CityComponent },
+   name : 'HelloWorld',
+   data(){
+        return {
+  cities: [
+    {
+      id: 1,
+      name: 'Ville 1',
+      weather: 'Ensoleillé',
+      temperature: 22.0,
+      updatedAt: new Date()
+    },
+    {
+      id: 2,
+      name: 'Ville 2',
+      weather: 'Peu nuageux',
+      temperature: 19.5,
+      updatedAt: new Date()
+    }
+  ]
+}
+    },
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/City.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="CityComponent">
+        <h3>{{ name  }}</h3>
+        <h3>{{ weather }}</h3>
+        <h3>{{ temperature }}</h3>
+        <h3>{{ updatedAt }}</h3>
+
+        <!-- <h2>{{ weather  }}</h2>
+        <h3>{{ temperature  }}</h3>
+        <h3>{{  updatedAt.toLocaleString()   }}</h3> -->
+    </div>
+</template>
+  
+<script>
+export default {
+  name: 'CityComponent',
+  props:{
+    'name' : String,
+    'weather' : String,
+    'temperature' : Number,
+    'updatedAt' : String,
+  },
+}
+</script>
+
+
+<style scoped>
+
+</style>
