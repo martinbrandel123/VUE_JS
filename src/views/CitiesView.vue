bb97e73579fdf744c6e36b2293f3b08bbdebf428
Martin BRANDEL <martin.brandel@Digital-Grenoble.local>
2022-04-27 10:13:05 +0200

    ajout exercice router

---
+++ src/views/CitiesView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <CityComponent/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import CityComponent from '@/components/CitiesList.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    CityComponent
+  }
+}
+</script>
